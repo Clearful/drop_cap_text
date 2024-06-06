7f8f6461bf98802f8b6c884d6a66cb36c79dcdb8
Jeffrey Bunn <jbunn87@gmail.com>
2024-06-05 18:43:25 -0700

    Try to fix overscaling issue

--- lib/drop_cap_text.dart
+++ lib/drop_cap_text.dart
@@ -126,7 +126,7 @@ class DropCapText extends StatelessWidget {
     TextSpan textSpan = TextSpan(
       text: parseInlineMarkdown ? null : restData,
       children: parseInlineMarkdown ? mdRest!.toTextSpanList() : null,
-      style: textStyle.apply(fontSizeFactor: MediaQuery.of(context).textScaler.scale(textStyle.fontSize ?? 14)),
+      style: textStyle.apply(fontSizeFactor: MediaQuery.textScalerOf(context).scale(textStyle.fontSize ?? 1)),
     );
 
     TextPainter textPainter = TextPainter(textDirection: textDirection, text: textSpan, textAlign: textAlign);
